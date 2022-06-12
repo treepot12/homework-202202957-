6529abed35e316f0603d27fe79e102f5876cf344
win_hyeon_lim <61348734+treepot12@users.noreply.github.com>
2022-06-12 21:17:02 +0900

    Add files via upload

---
+++ homework/covid1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "covid1.h"
+
+int rate(int x,int y) {
+	int result;
+	result = x / y;
+	return result;
+}
+
---
+++ homework/covid1.h
@@ -0,0 +1,6 @@
+#pragma once
+struct inf {
+	int korea, usa, india, brazil, france;
+};
+typedef struct inf Cont;
+int rate(int x,int y);
